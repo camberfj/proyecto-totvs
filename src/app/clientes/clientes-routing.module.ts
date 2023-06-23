fb9322cc960cbab32f44833807a5ce2dfd3c0036
Fran <franjc_17@hotmail.com>
2023-06-23 16:53:35 -0300

    Modifique el archivo agrendo el path:'', y el component: ListaClientesComponent

--- src/app/clientes/clientes-routing.module.ts
+++ src/app/clientes/clientes-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaClientesComponent } from './lista-clientes/lista-clientes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ListaClientesComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
