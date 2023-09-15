4f9f0489368c1b7a599999b5076308d005a8e486
Fran <franjc_17@hotmail.com>
2023-09-15 15:22:03 -0300

    Se creo el component container-dialog

---
+++ src/app/clientes/container-dialog/container-dialog.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-container-dialog',
+  templateUrl: './container-dialog.component.html',
+  styleUrls: ['./container-dialog.component.css']
+})
+export class ContainerDialogComponent {}
